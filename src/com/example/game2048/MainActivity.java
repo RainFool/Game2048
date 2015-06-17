8c12d2346c3e3b1cbdf893c52bd60426807029df
RainFool <rainfool1024@gmail.com>
2015-06-17 17:37:47 +0800

    加入计分功能

--- src/com/example/game2048/MainActivity.java
+++ src/com/example/game2048/MainActivity.java
@@ -1,34 +1,41 @@
 package com.example.game2048;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	private static MainActivity mainActivity;
 
 	Button btnBack;
 	GameView gameView;
+	TextView tvScore;
+
+	public MainActivity() {
+		mainActivity = this;
+	}
+	//单例模式获取本对象
+	public static MainActivity getInstance() {
+		return mainActivity;
+	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		gameView = (GameView) findViewById(R.id.gameView);
-
+		tvScore = (TextView) findViewById(R.id.tvScore);
+		
 		btnBack = (Button) findViewById(R.id.btn_back);
 		btnBack.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				gameView.backLastView();
-				Toast.makeText(MainActivity.this, "默认Toast样式",
-						Toast.LENGTH_SHORT).show();
-//				backLastView();
-
-
 			}
 		});
 	}
