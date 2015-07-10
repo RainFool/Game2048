b1a8b97ad6606cddea9343010e0f160bfe4ac829
RainFool <rainfool1024@gmail.com>
2015-07-10 17:23:11 +0800

    加入颜色变化
    在values中加入color.xml存储颜色，在card类中加入控制代码

--- gen/com/example/game2048/R.java
+++ gen/com/example/game2048/R.java
@@ -10,6 +10,22 @@ package com.example.game2048;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int color1024=0x7f04000a;
+        public static final int color128=0x7f040007;
+        public static final int color16=0x7f040004;
+        public static final int color2=0x7f040001;
+        public static final int color2048=0x7f04000b;
+        public static final int color256=0x7f040008;
+        public static final int color32=0x7f040005;
+        public static final int color4=0x7f040002;
+        /**  TODO
+         */
+        public static final int color512=0x7f040009;
+        public static final int color64=0x7f040006;
+        public static final int color8=0x7f040003;
+        public static final int colorDefault=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,31 +33,31 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int Score=0x7f080000;
-        public static final int action_settings=0x7f080005;
-        public static final int btn_again=0x7f080004;
-        public static final int btn_back=0x7f080003;
-        public static final int gameView=0x7f080002;
-        public static final int tvScore=0x7f080001;
+        public static final int Score=0x7f090000;
+        public static final int action_settings=0x7f090005;
+        public static final int btn_again=0x7f090004;
+        public static final int btn_back=0x7f090003;
+        public static final int gameView=0x7f090002;
+        public static final int tvScore=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int score=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int score=0x7f060003;
     }
     public static final class style {
         /** 
@@ -65,10 +81,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
